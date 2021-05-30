578dcc86cdbd56cedd8982bbf76a7fc1c321c39a
Chiranjibee Mohapatra <chiranjibeearyan@gmail.com>
2021-05-30 16:30:36 +0530

    adding

--- app.js
+++ app.js
@@ -139,6 +139,6 @@ let port = process.env.PORT;
 if (port == null || port == ""){
   port = 3000;
 };
-app.listen(3000, function() {
-  console.log("Server started Successfully");
+app.listen(port, function() {
+  console.log("Server has started Successfully");
 });
